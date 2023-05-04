c98fd5ca2e46d159032c49c95358d643bc36358e
DevShara <developer.ishara@gmail.com>
2023-05-04 11:30:17 +0530

    complete project section

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -1,26 +1,111 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
     return (
         <div className='text-white container mx-auto mt-12'>
            <h1>Projects</h1>
-           <div className='grid grid-cols-4'>
-                <div className='rounded-xl relative project-item'>
+           <div className='grid grid-cols-4 mt-12 gap-12'>
+                <motion.div
+                    className='rounded-xl relative project-item'
+                    whileHover={{ scale: 1.05,  opacity:0.9 }}
+                    transition={{ type: "spring", stiffness: 100, damping: 25 }}
+                    whileTap={{
+                        scale: 0.85,
+                       
+                    }}
+                >
                     <div className='absolute left-6 top-6'>
                         <h3 className=''>Web App</h3>
                         <h2 className='font-bold text-xl'>Employee Rewarder</h2>
                     </div>
-                    <div className='  w-full absolute bottom-0 py-4 backdrop-blur-sm  p-6 bg-black/30 flex  justify-between  items-center '>
-                        {/* <div className=' ml-6  '>
-                            <button className=' bg-white text-gray-800 px-4 py-2  font-semibold rounded-full'>Learn more</button>
-                        </div> */}
-                        <ul className=' flex gap-2 list-inside list-disc tech-stack-list  '>
+                    <div className='  w-full absolute bottom-0 py-4 backdrop-blur-sm rounded-b-xl  px-6 bg-black/40 flex flex-col  gap-3   '>
+                       
+                        <ul className='tech-stack-list  '>
                             <li className='tech-stack-item'>Next js</li>
                             <li className='tech-stack-item'>Tailwind Css</li>
                             <li className='tech-stack-item'>Sanity</li>
                         </ul>
+                        <div className=' '>
+                            <button className=' bg-white text-gray-800 px-4 py-2  font-semibold rounded-full'>Learn more</button>
+                        </div>
                     </div>
-                </div>
+                </motion.div>
+                <motion.div
+                    className='rounded-xl relative project-item'
+                    whileHover={{ scale: 1.05,  opacity:0.9 }}
+                    transition={{ type: "spring", stiffness: 100, damping: 25 }}
+                    whileTap={{
+                        scale: 0.85,
+                       
+                    }}
+                >
+                    <div className='absolute left-6 top-6'>
+                        <h3 className=''>Web App</h3>
+                        <h2 className='font-bold text-xl'>Employee Rewarder</h2>
+                    </div>
+                    <div className='  w-full absolute bottom-0 py-4 backdrop-blur-sm rounded-b-xl  px-6 bg-black/40 flex flex-col  gap-3   '>
+                       
+                        <ul className='tech-stack-list  '>
+                            <li className='tech-stack-item'>Next js</li>
+                            <li className='tech-stack-item'>Tailwind Css</li>
+                            <li className='tech-stack-item'>Sanity</li>
+                        </ul>
+                        <div className=' '>
+                            <button className=' bg-white text-gray-800 px-4 py-2  font-semibold rounded-full'>Learn more</button>
+                        </div>
+                    </div>
+                </motion.div>
+                <motion.div
+                    className='rounded-xl relative project-item'
+                    whileHover={{ scale: 1.05,  opacity:0.9 }}
+                    transition={{ type: "spring", stiffness: 100, damping: 25 }}
+                    whileTap={{
+                        scale: 0.85,
+                       
+                    }}
+                >
+                    <div className='absolute left-6 top-6'>
+                        <h3 className=''>Web App</h3>
+                        <h2 className='font-bold text-xl'>Employee Rewarder</h2>
+                    </div>
+                    <div className='  w-full absolute bottom-0 py-4 backdrop-blur-sm rounded-b-xl px-6 bg-black/40 flex flex-col  gap-3   '>
+                       
+                        <ul className='tech-stack-list  '>
+                            <li className='tech-stack-item'>Next js</li>
+                            <li className='tech-stack-item'>Tailwind Css</li>
+                            <li className='tech-stack-item'>Sanity</li>
+                        </ul>
+                        <div className=' '>
+                            <button className=' bg-white text-gray-800 px-4 py-2  font-semibold rounded-full'>Learn more</button>
+                        </div>
+                    </div>
+                </motion.div>
+                <motion.div
+                    className='rounded-xl relative project-item'
+                    whileHover={{ scale: 1.05,  opacity:0.9 }}
+                    transition={{ type: "spring", stiffness: 100, damping: 25 }}
+                    whileTap={{
+                        scale: 0.85,
+                       
+                    }}
+                >
+                    <div className='absolute left-6 top-6'>
+                        <h3 className=''>Web App</h3>
+                        <h2 className='font-bold text-xl'>Employee Rewarder</h2>
+                    </div>
+                    <div className='  w-full absolute bottom-0 py-4 backdrop-blur-sm rounded-b-xl  px-6 bg-black/40 flex flex-col  gap-3   '>
+                       
+                        <ul className='tech-stack-list  '>
+                            <li className='tech-stack-item'>Next js</li>
+                            <li className='tech-stack-item'>Tailwind Css</li>
+                            <li className='tech-stack-item'>Sanity</li>
+                        </ul>
+                        <div className=' '>
+                            <button className=' bg-white text-gray-800 px-4 py-2  font-semibold rounded-full'>Learn more</button>
+                        </div>
+                    </div>
+                </motion.div>
            </div>
         </div>
     );
