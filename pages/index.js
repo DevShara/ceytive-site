cd022997a82354b60b102632ff158da61334888d
DevShara <developer.ishara@gmail.com>
2023-05-08 14:45:20 +0530

    fix build error by moving components folder to the root

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 import { useEffect, useState } from 'react';
 
-import { ThemeContext } from './context';
+import { ThemeContext } from '../context';
 import { useContext } from "react";
-import Hero from './components/Hero';
-import SmoothScroll from './components/SmoothScroll';
-import About from './components/About';
-import TechSlider from './components/TechSlider';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Hero from '../components/Hero';
+import SmoothScroll from '../components/SmoothScroll';
+import About from '../components/About';
+import TechSlider from '../components/TechSlider';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 
 
